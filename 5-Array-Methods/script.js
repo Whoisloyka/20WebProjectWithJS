49b1e3a8383150fa8176b957c46650ec7ab04153
Whoisloyka <whoisloyka@gmail.com>
2022-06-22 18:05:39 +0300

    Only reduce func will be added

--- 5-Array-Methods/script.js
+++ 5-Array-Methods/script.js
@@ -34,8 +34,56 @@ async function getRandomUser() {
   addData(newUser);
 }
 
+// Add new obj to data array
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
 
-console.log(data);
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+// Double the money
+function doubleMoney() {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
+}
+
+// Sort users by richest
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
+
+// Show only millionaires
+function showMillionaires() {
+  data = data.filter((user) => user.money > 1000000);
+
+  updateDOM();
+}
+
+// Event listeners
+addUserBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
