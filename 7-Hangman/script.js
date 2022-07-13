680424bdaa27a41f5ff3a90fdcf16acbe81e606a
Whoisloyka <whoisloyka@gmail.com>
2022-07-13 18:26:22 +0300

    continue from update wrong letters

--- 7-Hangman/script.js
+++ 7-Hangman/script.js
@@ -10,36 +10,73 @@ const figureParts = document.getElementById("figure-part");
 const words = ["application", "programming", "interface", "wizard"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
-
+console.log(selectedWord);
 let playable = true;
 
-const correctLetters = ["r"];
+const correctLetters = [];
 const wrongLetters = [];
 
 // Show hidden word
 function displayWord() {
+  /** Tur a string into an array **/
   wordEl.innerHTML = `
-  ${selectedWord
-    .split("")
-    .map(
-      (letter) => `
-  <span class="letter">
-    ${correctLetters.includes(letter) ? letter : ""}
-  </span>
-`
-    )
-    .join("")}
-  `;
-
-  const innerWord = wordEl.innerText.replace(/[ \n]/g, "");
+        ${selectedWord
+          .split("")
+          .map(
+            (letter) => `
+        <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ""}
+        </span>
+        `
+          )
+          // turn back into a string
+          .join("")}
+    `;
+  // stringe dönüştürdüğün harfleri yanyana dizip kelime haline getir.
+  const innerWord = wordEl.innerText.replace(/\n/g, "");
 
   if (innerWord === selectedWord) {
-    finalMessage.innerText = "Congratulations! You won! 😃";
-    finalMessageRevealWord.innerText = "";
+    finalMessage.innerText = "Congratulations! You Won!";
     popup.style.display = "flex";
-
-    playable = false;
   }
 }
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+  console.log("Update wrong");
+}
+
+// Show notification
+function showNotification() {
+  notification.classList.add("show");
+
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 3000);
+}
+
+//KeyDown letter press
+window.addEventListener("keydown", (e) => {
+  // Yalnızca a-z arası harfleri kabul edeceğiz. Only accept the letters in which the range of a-z.
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
 
 displayWord();
