810ecff6cdd402009f3646d485b02116eb7f3b2d
Whoisloyka <whoisloyka@gmail.com>
2022-07-18 09:12:06 +0300

    Done!

--- 7-Hangman/script.js
+++ 7-Hangman/script.js
@@ -1,16 +1,148 @@
+// const wordEl = document.getElementById("word");
+// const wrongLettersEl = document.getElementById("wrong-letters");
+// const playAgainBtn = document.getElementById("play-button");
+// const popup = document.getElementById("popup-container");
+// const notification = document.getElementById("notification-container");
+// const finalMessage = document.getElementById("final-message");
+
+// const figureParts = document.getElementById("figure-part");
+
+// const words = ["application", "programming", "interface", "wizard"];
+
+// let selectedWord = words[Math.floor(Math.random() * words.length)];
+// console.log(selectedWord);
+// let playable = true;
+
+// const correctLetters = [];
+// const wrongLetters = [];
+
+// // Show hidden word
+// function displayWord() {
+//   /** Tur a string into an array **/
+//   wordEl.innerHTML = `
+//         ${selectedWord
+//           .split("")
+//           .map(
+//             (letter) => `
+//         <span class="letter">
+//         ${correctLetters.includes(letter) ? letter : ""}
+//         </span>
+//         `
+//           )
+//           // turn back into a string
+//           .join("")}
+//     `;
+//   // stringe dönüştürdüğün harfleri yanyana dizip kelime haline getir.
+//   const innerWord = wordEl.innerText.replace(/\n/g, "");
+
+//   if (innerWord === selectedWord) {
+//     finalMessage.innerText = "Congratulations! You Won!";
+//     popup.style.display = "flex";
+//   }
+// }
+
+// // Update the wrong letters
+// function updateWrongLettersEl() {
+//   // Display wrong letters
+//   wrongLettersEl.innerHTML = `
+//     ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+//     ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+//   `;
+
+//   // Display body part
+//   figureParts.forEach((part, index) => {
+//     const errors = wrongLetters.length;
+
+//     if (index < errors) {
+//       part.style.display = "block";
+//     } else {
+//       part.style.display = "none";
+//     }
+//   });
+
+//   // figureParts.forEach((part, index) => {
+//   //   const errors = wrongLetters.length;
+
+//   //   if (index < errors) {
+//   //     part.style.display = "block";
+//   //   } else {
+//   //     part.style.display = "none";
+//   //   }
+//   // });
+
+//   // Check if lost
+//   if (wrongLetters.length === figureParts.length) {
+//     finalMessage.innerText = "Kaybettiniz.";
+//     popup.style.display = "flex";
+//   }
+// }
+
+// // Show notification
+// function showNotification() {
+//   notification.classList.add("show");
+
+//   setTimeout(() => {
+//     notification.classList.remove("show");
+//   }, 2000);
+// }
+
+// //KeyDown letter press
+// window.addEventListener("keydown", (e) => {
+//   // Yalnızca a-z arası harfleri kabul edeceğiz. Only accept the letters in which the range of a-z.
+//   if ((e.keyCode >= 65 && e.keyCode <= 90) || e.keyCode == 222) {
+//     const letter = e.key;
+
+//     if (selectedWord.includes(letter)) {
+//       if (!correctLetters.includes(letter)) {
+//         correctLetters.push(letter);
+//         displayWord();
+//       } else {
+//         showNotification();
+//       }
+//     } else {
+//       if (!wrongLetters.includes(letter)) {
+//         wrongLetters.push(letter);
+//         updateWrongLettersEl();
+//       } else {
+//         showNotification();
+//       }
+//     }
+//   }
+// });
+
+// // Restart and play again
+// playAgainBtn.addEventListener("click", () => {
+//   // Empty arrays
+//   correctLetters.splice(0);
+//   wrongLetters.splice(0);
+
+//   selectedWord = words[Math.floor(Math.random() * words.length)];
+
+//   displayWord();
+
+//   updateWrongLettersEl();
+
+//   popup.style.display = "none";
+// });
+
+// displayWord();
+
 const wordEl = document.getElementById("word");
 const wrongLettersEl = document.getElementById("wrong-letters");
-const playAgainBtn = document.getElementById("play-again");
+const playAgainBtn = document.getElementById("play-button");
 const popup = document.getElementById("popup-container");
 const notification = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
+const finalMessageRevealWord = document.getElementById(
+  "final-message-reveal-word"
+);
 
-const figureParts = document.getElementById("figure-part");
+const figureParts = document.querySelectorAll(".figure-part");
 
 const words = ["application", "programming", "interface", "wizard"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
-console.log(selectedWord);
+
 let playable = true;
 
 const correctLetters = [];
@@ -18,32 +150,57 @@ const wrongLetters = [];
 
 // Show hidden word
 function displayWord() {
-  /** Tur a string into an array **/
   wordEl.innerHTML = `
-        ${selectedWord
-          .split("")
-          .map(
-            (letter) => `
-        <span class="letter">
-        ${correctLetters.includes(letter) ? letter : ""}
-        </span>
+    ${selectedWord
+      .split("")
+      .map(
+        (letter) => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ""}
+          </span>
         `
-          )
-          // turn back into a string
-          .join("")}
-    `;
-  // stringe dönüştürdüğün harfleri yanyana dizip kelime haline getir.
-  const innerWord = wordEl.innerText.replace(/\n/g, "");
+      )
+      .join("")}
+  `;
+
+  const innerWord = wordEl.innerText.replace(/[ \n]/g, "");
 
   if (innerWord === selectedWord) {
-    finalMessage.innerText = "Congratulations! You Won!";
+    finalMessage.innerText = "Congratulations! You won! 😃";
+    finalMessageRevealWord.innerText = "";
     popup.style.display = "flex";
+
+    playable = false;
   }
 }
 
 // Update the wrong letters
 function updateWrongLettersEl() {
-  console.log("Update wrong");
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+
+  // Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
+    }
+  });
+
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Unfortunately you lost. 😕";
+    finalMessageRevealWord.innerText = `...the word was: ${selectedWord}`;
+    popup.style.display = "flex";
+
+    playable = false;
+  }
 }
 
 // Show notification
@@ -52,31 +209,51 @@ function showNotification() {
 
   setTimeout(() => {
     notification.classList.remove("show");
-  }, 3000);
+  }, 2000);
 }
 
-//KeyDown letter press
+// Keydown letter press
 window.addEventListener("keydown", (e) => {
-  // Yalnızca a-z arası harfleri kabul edeceğiz. Only accept the letters in which the range of a-z.
-  if (e.keyCode >= 65 && e.keyCode <= 90) {
-    const letter = e.key;
+  if (playable) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+      const letter = e.key.toLowerCase();
 
-    if (selectedWord.includes(letter)) {
-      if (!correctLetters.includes(letter)) {
-        correctLetters.push(letter);
-        displayWord();
+      if (selectedWord.includes(letter)) {
+        if (!correctLetters.includes(letter)) {
+          correctLetters.push(letter);
+
+          displayWord();
+        } else {
+          showNotification();
+        }
       } else {
-        showNotification();
-      }
-    } else {
-      if (!wrongLetters.includes(letter)) {
-        wrongLetters.push(letter);
-        updateWrongLettersEl();
-      } else {
-        showNotification();
+        if (!wrongLetters.includes(letter)) {
+          wrongLetters.push(letter);
+
+          updateWrongLettersEl();
+        } else {
+          showNotification();
+        }
       }
     }
   }
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener("click", () => {
+  playable = true;
+
+  //  Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = "none";
 });
 
 displayWord();
