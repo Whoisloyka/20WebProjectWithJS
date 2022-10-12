8192bd811b4cfa0a041d47d9bef9bfaf36afc7f9
Whoisloyka <whoisloyka@gmail.com>
2022-10-12 17:27:36 +0300

    Music Player Done

---
+++ 10- Music Player/script.js
@@ -0,0 +1,114 @@
+const musicContainer = document.getElementById("music-container")
+const playBtn = document.getElementById("play")
+const prevBtn = document.getElementById("prev")
+const nextBtn = document.getElementById("next")
+const audio = document.getElementById("audio")
+const progress = document.getElementById("progress")
+const progressContainer = document.getElementById("progress-container")
+const title = document.getElementById("title")
+const cover = document.getElementById("cover")
+
+// Song title
+const songs = ["hey", "summer", "ukulele"]
+
+// Keep track of songs
+let songIndex = 1
+
+// Initially load song details into DOM
+loadSong(songs[songIndex])
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song
+  audio.src = `music/${song}.mp3`
+  cover.src = `images/${song}.jpg`
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add("play")
+  playBtn.querySelector("i.fas").classList.remove("fa-play")
+  playBtn.querySelector("i.fas").classList.add("fa-pause")
+  audio.play()
+}
+
+// Pause song
+function pauseSong() {
+  if (musicContainer.classList.contains("play")) {
+    audio.pause()
+    musicContainer.classList.remove("play")
+    playBtn.querySelector("i.fas").classList.remove("fa-pause")
+    playBtn.querySelector("i.fas").classList.add("fa-play")
+  }
+}
+
+// Previous song
+function prevSong() {
+  songIndex--
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1
+  }
+
+  loadSong(songs[songIndex])
+  if (musicContainer.classList.contains("play")) {
+    audio.play()
+  }
+}
+
+// Next song
+function nextSong() {
+  songIndex++
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0
+  }
+
+  loadSong(songs[songIndex])
+  if (musicContainer.classList.contains("play")) {
+    audio.play()
+  }
+}
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement
+  const progressPercent = (currentTime / duration) * 100
+  progress.style.width = `${progressPercent}%`
+  //   if (currentTime === duration) {
+  //     nextSong()
+  //   }
+}
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth
+  const clickX = e.offsetX
+  const duration = audio.duration
+  audio.currentTime = (clickX / width) * duration
+  console.log(clickX, width, duration)
+}
+
+// Event listeners
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play")
+
+  if (isPlaying) {
+    pauseSong()
+  } else {
+    playSong()
+  }
+})
+
+// Change Song
+prevBtn.addEventListener("click", prevSong)
+nextBtn.addEventListener("click", nextSong)
+
+// Time/song update
+audio.addEventListener("timeupdate", updateProgress)
+
+// Click on progress bar
+progressContainer.addEventListener("click", setProgress)
+
+// When song ends
+audio.addEventListener("ended", nextSong)
---
+++ 11- Infinite-Scroll-Post/script.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+let list = new ListNode(0)
+var addTwoNumbers = function (l1, l2) {
+  if (l1 === null || l2 === null) return null
+
+  // using dummyHead can simplify linked list's calculation, dummyHead.next points to the new linked list
+  let dummyHead = new ListNode(0)
+  let cur1 = l1
+  let cur2 = l2
+  let cur = dummyHead // cur is for the calculation in new linked list
+  let carry = 0 // carry-over symbol
+
+  while (cur1 !== null || cur2 !== null) {
+    let val1 = cur1 !== null ? cur1.val : 0
+    let val2 = cur2 !== null ? cur2.val : 0
+    let sum = val1 + val2 + carry
+    let newNode = new ListNode(sum % 10) // the result of sum%10 ranges from 0 to 9, which is the value of the current digit
+    carry = sum >= 10 ? 1 : 0 // sum>=10, carry=1, so carry-over exists here
+    cur.next = newNode
+    cur = cur.next
+
+    if (cur1 !== null) {
+      cur1 = cur1.next
+    }
+
+    if (cur2 !== null) {
+      cur2 = cur2.next
+    }
+  }
+
+  if (carry > 0) {
+    // If there's still carry-over in the end, then add a new node
+    cur.next = new ListNode(carry)
+  }
+
+  return dummyHead.next
+}
+// addTwoNumbers(l1, l2)
